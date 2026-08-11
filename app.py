fd2b8328a6b608f37f1015ab57fc7a2485122226
JosephThia <121495798+JosephThia@users.noreply.github.com>
2026-08-11 03:18:07 +0000

    1

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 # DBS Prediction
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET",'POST'])
+def index():
+    return('hi')
+
+# the following won't be run in the cloud (which is the app)
+if __name__ == "__main__":
+    app.run()
