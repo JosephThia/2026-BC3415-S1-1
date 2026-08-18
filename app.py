1a772797938fb9d6bfa5f7ac9247c18d83c746fa
JosephThia <121495798+JosephThia@users.noreply.github.com>
2026-08-18 03:22:22 +0000

    1

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html")) # trying to retrieve the information from the index.html template
 
+@app.route("/main", methods=["GET",'POST'])
+def main():
+    return(render_template("main.html")) # trying to retrieve the information from the main.html template
+
 # the following won't be run in the cloud (which is the app)
 if __name__ == "__main__":
     app.run()
