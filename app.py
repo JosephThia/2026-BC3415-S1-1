d9e1c82d4c14c881be5c017ac3cfbf1ff7fc7aa9
JosephThia <121495798+JosephThia@users.noreply.github.com>
2026-08-25 02:31:27 +0000

    1

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 # DBS Prediction - V2
 
 from flask import Flask, render_template, request
+import joblib
+
+modelPKL = joblib.load("DBS_SingDollar.pkl")
 
 app = Flask(__name__)
 
@@ -16,6 +19,12 @@ def main():
 def dbs():
     return(render_template("dbs.html")) 
 
+@app.route("/DBSresult",methods=["GET",'POST'])
+def DBSresult():
+    q = float(request.form.get("q"))
+    r = modelPKL.predict([[q]])
+    return(render_template("DBSresult.html",r=r)) 
+
 # the following won't be run in the cloud (which is the app)
 if __name__ == "__main__":
-    app.run()
+    app.run(port=1234)
